435038a8fb5cd84cacadb22e72785e300901302a
Lucjan <lucjan.kepinski@gmail.com>
2019-10-25 14:36:55 +0200

    traversing dom with jquery

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-$("#lead-banner p");
+// $("#lead-banner p");
 
-$("h3").css({border: "3px solid blue"})
+// $("h3").css({border: "3px solid blue"})
 
-$(".wrapper").css({border: "1px solid red"})
+// $(".wrapper").css({border: "1px solid red"})
 
-$("#clients").css({border: "3px solid yellow"})
+// $("#clients").css({border: "3px solid yellow"})
 
-$("#contact ul:first-child").css({border: "3px solid red"})
-$("#contact ul:last-child").css({border: "3px solid red"})
+// $("#contact ul:first-child").css({border: "3px solid red"})
+// $("#contact ul:last-child").css({border: "3px solid red"})
 
-$("header nav li:odd").css({border: "3px solid orange"})
+// $("header nav li:odd").css({border: "3px solid orange"})
 
-$("section:not('#contact')").css({border: "3px solid blue"})
+// $("section:not('#contact')").css({border: "3px solid blue"})
 
-$("#social-nav li:gt(2)").css({border: "3px solid purple"})
+// $("#social-nav li:gt(2)").css({border: "3px solid purple"})
 
-$("img[alt]").css({border: "3px solid pink"})
+// $("img[alt]").css({border: "3px solid pink"})
 
-$("img[alt=quote]").css({border: "3px solid yellow"})
+// $("img[alt=quote]").css({border: "3px solid yellow"})
+
+$("#contact-methods").next().css({border: "3px solid red"})
+
+$("#social-nav").prev().css({border: "3px solid blue"})
+
+$(".banner-title").parents().css({border: "3px solid pink"})
+
+$("#social-nav").children().css({border: "3px solid green"})
+
+$("#contact").find(".facebook").css({border: "3px solid purple"})
+
+$("#social-nav").closest(".wrapper").css({border: "3px solid orange"})
