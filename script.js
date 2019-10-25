b032ccf3ba7c4619a92f44608d449d3532dd7503
Lucjan <lucjan.kepinski@gmail.com>
2019-10-25 14:23:07 +0200

    jquery filters

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ $("h3").css({border: "3px solid blue"})
 $(".wrapper").css({border: "1px solid red"})
 
 $("#clients").css({border: "3px solid yellow"})
+
+$("#contact ul:first-child").css({border: "3px solid red"})
+$("#contact ul:last-child").css({border: "3px solid red"})
+
+$("header nav li:odd").css({border: "3px solid orange"})
+
+$("section:not('#contact')").css({border: "3px solid blue"})
+
+$("#social-nav li:gt(2)").css({border: "3px solid purple"})
+
+$("img[alt]").css({border: "3px solid pink"})
+
+$("img[alt=quote]").css({border: "3px solid yellow"})
